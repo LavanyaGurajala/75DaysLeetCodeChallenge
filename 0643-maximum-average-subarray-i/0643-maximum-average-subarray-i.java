2ffac605edb610529e6d559dd9696d1ada850b8d
Gurajala Lavanya <2300033427@kluniversity.in>
2026-03-26 20:12:16 +0530

    Time: 3 ms (81.93%), Space: 69.4 MB (84.96%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -0,0 +1,15 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int s=0;
+        for(int i=0;i<k;i++){
+            s+=nums[i];
+        }
+        int max=s;
+        for(int i=k;i<nums.length;i++){
+            s+=nums[i];
+            s-=nums[i-k];
+            max=Math.max(max,s);
+        }
+        return (double)max/k;
+    }
+}
